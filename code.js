f6c3217209973d038faf7df999c89bce095f62e8
mnowak16@my.wctc.edu <Cr34t!ve6>
2022-04-06 20:09:26 -0500

    final update

--- code.js
+++ code.js
@@ -6,26 +6,39 @@ let beers = 0;
 let wine = 0;
 let liquor = 0;
 let hours = 0;
+let bac = 0;
 
 function processForm(event) {
     event.preventDefault();
+    getVariables();
+    calcBAC(weight, beers, wine, liquor, hours);
+    moveSlider();
+    returnText();
+}
 
+function getVariables() {
     //get weight, beers, wine, liquor, and hours from input box
     weight = $("input#weight").val();
     beers = $("input#beers").val();
     wine = $("input#wine").val();
     liquor = $("input#liquor").val();
     hours = $("input#hours").val();
+    return weight, beers, wine, liquor, hours;
+}
 
+function calcBAC(weight, beers, wine, liquor, hours) {
     //round BAC to 3 decimal places
     let totalLiquidOunces = (0.54 * beers) + (0.6 * wine) + (0.6 * liquor);
     let absorbRate = 7.5 * totalLiquidOunces;
-    let BAC = ((absorbRate / weight) - (0.015 * hours)).toFixed(3);
-    Number(BAC);
-
-    //move the slider to match the number
-    $("input#slider").val(BAC);
-
-    $("p#BAC").text(`BAC: ${BAC}%`);
+    bac = ((absorbRate / weight) - (0.015 * hours)).toFixed(3);
+    return Number(bac);
 }
 
+function moveSlider() {
+    //move the slider to match the number
+    $("input#slider").val(bac);
+}
+
+function returnText() {
+    $("p#BAC").text(`BAC: ${bac}%`);
+}
